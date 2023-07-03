22467a64ce666cc2715eedb03d4c6b411c00181a
hana <jjss77777@naver.com>
2023-07-03 14:34:27 +0900

    3진법뒤집기

--- src/Programmers_Lv1/삼진법뒤집기.java
+++ src/Programmers_Lv1/삼진법뒤집기.java
@@ -1,29 +1,27 @@
 package Programmers_Lv1;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class 삼진법뒤집기 {
     public static void main(String[] args) {
-        int[] arr = {1,1,3,3,0,1,1};
-        System.out.println("시작할게:"+solution(arr));
+        int a = 45;
+
+        System.out.println(solution(a));
     }
     // E2, s2, w1
-    public static int[] solution(int []arr) {
-        int[] answer = {};
-        ArrayList<Integer> answerList=new ArrayList<>();
+    public static int solution(int a) {
+        String ternary = Integer.toString(a,3); // 3진법으로 변환
+        String reverse  = ""; // 뒤집은 수를 담기
+        int answer=0;
 
-        int tmp=-1;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] != tmp) {
-                answerList.add(arr[i]);
-                tmp=arr[i];
-            }
+        for(int i=ternary.length()-1; i>=0; i--){
+            reverse += ternary.charAt(i);
         }
+        System.out.println(reverse);
 
-        answer = new int[answerList.size()];
-        for(int i=0; i<answerList.size(); i++){
-            answer[i] = answerList.get(i);
-        }
+        answer = Integer.parseInt(reverse,3);
+
         return answer;
     }
 }
