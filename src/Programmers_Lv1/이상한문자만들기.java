ec72bb07987a450ffc9dfd55e43241c00f194d9d
hanaxauay <hana0_0aa@naver.com>
2023-07-05 01:43:22 +0900

    이상한문자만들기

---
+++ src/Programmers_Lv1/이상한문자만들기.java
@@ -0,0 +1,15 @@
+package Programmers_Lv1;
+
+
+
+public class 이상한문자만들기 {
+    public static void main(String[] args) {
+        int a = 45;
+
+        System.out.println(solution(a));
+    }
+    public static int solution(int a) {
+
+        return answer;
+    }
+}
